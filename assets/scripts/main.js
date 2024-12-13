1fe7b376a6b590caab03b6c5b74b1af709bc17b2
Cent38jrjiawfiadfjidaf <esplana.vincent29@gmail.com>
2024-12-13 20:58:12 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+spanHi.style.color = "red"
+spanBye.style.backgroundColor = "blue"
